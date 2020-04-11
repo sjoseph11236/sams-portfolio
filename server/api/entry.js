5de618cf2cd6654e56b078a49962c690e9960518
Sayeed Joseph <sayeedjoseph@gmail.com>
2020-04-11 16:49:35 -0400

    chore: add put and delete route

--- server/api/entry.js
+++ server/api/entry.js
@@ -14,9 +14,32 @@ router.get('/', async(req, res, next) => {
 router.post('/', async (req, res, next) => {
   try {
     const entry = { ...req.body, userId: 1 }
-    console.log('entry',entry);
     const createdEntry = await Entry.create(entry);
     res.send(createdEntry);
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+router.put('/:id', async (req, res, next) => { 
+  try {
+    const body = req.body; 
+    const id = req.params.id;
+    await Entry.update(body, { where: { id }});
+    const entry  = await Entry.findByPk(id);
+    res.send(entry)
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+router.delete('/:id', async (req, res, next) => { 
+  try {
+    const id = req.params.id
+    await Entry.destroy({ where: { id }});;
+    res.send({});
   } catch (error) {
     console.error(error);
     next(error);
